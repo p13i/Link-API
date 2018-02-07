9f54cbaa20cd76601096bdd51ca410bff8031db5
Pramod Kotipalli <pramod.kotipalli@gmail.com>
2018-02-07 12:31:57 -0500

    Add cors headers

--- api/api/settings.py
+++ api/api/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
     # Third-party applications
     'rest_framework',
+    'corsheaders',
 
     # Django apps
     'link',
@@ -54,12 +55,15 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'api.urls'
 
